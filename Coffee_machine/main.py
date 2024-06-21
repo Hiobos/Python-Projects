b5b782e5c280241dfe63b31a889112b4fa054bfc
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2024-06-21 23:14:15 +0200

    updated main.py
    added close to nothing, will do more tommorow

--- Coffee_machine/main.py
+++ Coffee_machine/main.py
@@ -1,2 +1,26 @@
-from menu import MENU
-product = input("What type of coffee do u want? (espresso/latte/cappuccino)")
+from menu import MENU, resources
+product = input("What type of coffee do u want? (espresso/latte/cappuccino)").lower()
+
+machine = True
+
+def coffee_make():
+    global machine
+    if product == 'report':
+        print(resources)
+    elif product == 'off':
+        machine = False
+    elif product in MENU:
+        if resources['water'] > MENU[product]['resources']['water']:
+            if resources['coffee'] > MENU[product]['resources']['coffee']:
+                if resources['milk'] > MENU[product]['resources']['milk'] or ['milk'] not in MENU[product]['resources']:
+                    pass #robi kawe
+                else:
+                    print("Not enough milk")
+            else:
+                print('Not enough coffee')
+        else:
+            print('Not enough water')
+
+
+while machine:
+    coffee_make()
