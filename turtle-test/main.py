8dbdd331d6afa0e6a33abc8b672082335e73bf6b
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2024-07-01 21:48:25 +0200

    fixes
    fix

--- turtle-test/main.py
+++ turtle-test/main.py
@@ -1,9 +1,12 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+#
+# zolw = Turtle()
+# zolw.shape('turtle')
+# zolw.color('crimson')
+# zolw.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-
-zolw = Turtle()
-zolw.shape('turtle')
-zolw.color('crimson')
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
