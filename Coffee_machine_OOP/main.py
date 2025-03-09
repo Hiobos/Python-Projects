18ad453b4afff2ab947b198cc471d5ababbc4e6b
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-09 19:12:31 +0100

    Completed Coffee machine

--- Coffee_machine_OOP/main.py
+++ Coffee_machine_OOP/main.py
@@ -2,7 +2,27 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffe_maker = CoffeeMaker()
+menu = Menu()
+
 machine = True
+options = ["espresso", "latte", "cappuccino", "off", "report"]
+
+
 
 while machine:
-    drink = "What would you like? (espresso/latte/cappuccino/): "
+    choice = input(f"What would you like? espresso/latte/cappuccino: ").lower()
+    if choice in options:
+        if choice == 'off':
+            machine = False
+        elif choice == 'report':
+            coffe_maker.report()
+            money_machine.report()
+        else:
+            drink = menu.find_drink(choice)
+            if coffe_maker.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffe_maker.make_coffee(drink)
+    else:
+        print("invalid option.")
