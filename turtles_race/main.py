d99bea50190016d6a2dd65357df09ef660d75c82
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-12 12:39:16 +0100

    Completed turtle race game
    Added:
    -all turtles are moving now
    -finish line parameter
    -checks if turtle passed finish line
    -prints which turtle won
    -prints if the player won the bet

--- turtles_race/main.py
+++ turtles_race/main.py
@@ -4,21 +4,40 @@ import random
 screen = Screen()
 screen.setup(width=500, height=400)
 
+race_on = False
+
 bet = screen.textinput("Make a BET", "Which turtle will win?: ")
 
 colors = ["red", "orange", "green", "yellow", "blue", "black"]
 positions = [-70, -40, -10, 20, 50, 80]
+turtles = []
 
 for turtle_index in range(0, 6):
-    turt = Turtle(shape="turtle")
-    turt.color(colors[turtle_index])
-    turt.penup()
-    turt.goto(-230, positions[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(-230, positions[turtle_index])
+    turtles.append(new_turtle)
 
-game = True
-while game:
-    turt.forward(random.randint(0, 3))
+if bet:
+    race_on = True
+
+
+
+while race_on:
+    for turtle in turtles:
+        random_speed = random.randint(0, 10)
+        turtle.forward(random_speed)
+        if turtle.pos()[0] >= 230:
+            print(f"Turtle {turtle.color()[0]} won")
+            if turtle.color()[0] == bet:
+                print("You won the bet!")
+            else:
+                print("Better luck next time!")
+            race_on = False
     
+
+
 
 
 
