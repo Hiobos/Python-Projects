51ab51da9ab7916df9d801620c75921ac97924c6
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-12 21:09:31 +0100

    fixes

--- snake_game/main.py
+++ snake_game/main.py
@@ -7,13 +7,21 @@ screen.title("Snake Game")
 
 snake_body = []
 
-starting_position_x = [0, -20, -40]
-for index in range(0, 2):
-    snake_chunk = Turtle()
-    snake_chunk.shape('square')
+starting_position_x = [(0, 0), (-20, 0), (-40, 0)]
+for index in range(0, 3):
+    snake_chunk = Turtle(shape='square')
+    screen.tracer(2, 0)
     snake_chunk.color('green')
-    snake_chunk.position()
+    snake_chunk.penup()
+    snake_chunk.goto(starting_position_x[index])
+    snake_body.append(snake_chunk)
+
+game = True
+
+while game:
+    for chunk in snake_body:
+        chunk.forward(10)
 
 
-
+screen.tracer(1, 0)
 screen.exitonclick()
