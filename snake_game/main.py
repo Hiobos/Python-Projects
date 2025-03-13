08be3fa123b19a8cf3433f97a92c4e1b7ded05cb
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-13 13:19:03 +0100

    Added control mechanics to Snake
    now it moves in each of four directions by using arrow keys, also does not allow to go backwards (in it's own body)

--- snake_game/main.py
+++ snake_game/main.py
@@ -9,14 +9,18 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+screen.listen()
 
 game = True
 while game:
     screen.update()
-    time.sleep(0.1)
-
+    time.sleep(0.05)
     snake.move()
-    
+
+    screen.onkeypress(key='Left', fun=snake.move_left)
+    screen.onkeypress(key='Right', fun=snake.move_right)
+    screen.onkeypress(key='Up', fun=snake.move_up)
+    screen.onkeypress(key='Down', fun=snake.move_down)
 
 
 screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-
+MOVE_DISTANCE = 20
 
 class Snake:
     def __init__(self):
@@ -15,10 +15,28 @@ class Snake:
             snake_chunk.penup()
             snake_chunk.goto(index)
             self.snake_body.append(snake_chunk)
+            self.snake_body[0].color('white')
 
     def move(self):
         for chunk in range(len(self.snake_body) -1, 0, -1):
             new_x = self.snake_body[chunk - 1].xcor()
             new_y = self.snake_body[chunk - 1].ycor()
             self.snake_body[chunk].goto(new_x, new_y)
-            self.snake_body[0].forward(10)
+        self.snake_body[0].forward(MOVE_DISTANCE)
+
+    def move_right(self):
+        if self.snake_body[0].heading() != 180.0:
+            self.snake_body[0].setheading(0)
+
+    def move_left(self):
+        if self.snake_body[0].heading() != 0.0:
+            self.snake_body[0].setheading(180)
+
+    def move_up(self):
+        if self.snake_body[0].heading() != 270.0:
+            self.snake_body[0].setheading(90)
+
+    def move_down(self):
+        if self.snake_body[0].heading() != 90.0:
+            self.snake_body[0].setheading(270)
+
