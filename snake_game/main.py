ef49ee79b1282e7fac6431a3653fe953e9c00da6
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-14 08:56:21 +0100

    simplified snake
    changed tail collision

--- snake_game/main.py
+++ snake_game/main.py
@@ -8,9 +8,11 @@ screen = Screen()
 screen.setup(1000, 1000)
 screen.bgcolor('black')
 screen.title("Snake Game")
+#turns off animations
 screen.tracer(0)
 
 snake = Snake()
+#screen listening for key presses
 screen.listen()
 screen.onkeypress(key='Left', fun=snake.move_left)
 screen.onkeypress(key='Right', fun=snake.move_right)
@@ -20,26 +22,29 @@ screen.onkeypress(key='Down', fun=snake.move_down)
 score = Score()
 food = Food()
 
+#main loop
 game = True
 while game:
     screen.update()
     time.sleep(0.05)
-    #food
     snake.move()
 
+    #detects collision with food
     if snake.head.distance(food) < 15:
         #passed_score += 1
         snake.add_chunk()
         food.new_food()
         score.update_score()
 
-    if snake.head.xcor() > 480 or snake.head.xcor() < -480 or snake.head.ycor() > 480 or snake.head.ycor() < -480:
+    #detects collision with walls
+    if snake.head.xcor() > 490 or snake.head.xcor() < -490 or snake.head.ycor() > 490 or snake.head.ycor() < -490:
         score.game_over()
         game = False
 
 
-    for i in range(1, len(snake.snake_body)):
-        if snake.head.distance(snake.snake_body[i]) < 10:
+    #detects collision of snake head with its body
+    for i in snake.snake_body[1:]:
+        if snake.head.distance(i) < 10:
             score.game_over()
             game = False
 
