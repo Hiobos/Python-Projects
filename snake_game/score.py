500897b6d1dfc937cecc8027b14d4575ea70d2e3
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-18 11:58:59 +0100

    Added saving high score locally to snake game

--- snake_game/score.py
+++ snake_game/score.py
@@ -1,15 +1,19 @@
 from turtle import Turtle
+import os
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open ("high_score.txt", "r") as high_score:
+            self.high_score = high_score.read()
+
         self.color('white')
         self.penup()
         self.goto(0, 470)
-        self.write(f"Current Score: 0", move=False, align="center", font=('verdana', 20, 'normal'))
+        self.write(f"Current Score: {self.score} High Score {self.high_score}", move=False, align="center", font=('verdana', 20, 'normal'))
         self.hideturtle()
+
 
     def update_score(self):
         self.clear()
@@ -21,8 +25,14 @@ class Score(Turtle):
         self.write(f"Current Score: {self.score} High Score {self.high_score}", move=False, align="center", font=('verdana', 20, 'normal'))
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
+            print(f"{self.score} jest wyższe od {self.high_score}")
+
+            with open("high_score.txt", "w") as score1:
+                score1.write(str(self.score))
+
             self.high_score = self.score
+
         self.score = 0
         self.update_score()
 
