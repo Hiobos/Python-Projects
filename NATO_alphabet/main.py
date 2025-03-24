a0a3c7bca074b65706607802123d8996815fcb9f
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-03-24 12:06:26 +0100

    Nato alphabet added and completed

---
+++ NATO_alphabet/main.py
@@ -0,0 +1,17 @@
+
+#opening the file and creating dictionary
+with open('nato_phonetic_alphabet.csv') as f:
+    nato_alphabet = f.readlines()
+    nato_alphabet = [item.strip('\n') for item in nato_alphabet]
+    nato_alphabet = [item.split(',') for item in nato_alphabet]
+    nato_alphabet.pop(0)
+    nato_dict = {item[0]:item[1] for item in nato_alphabet}
+
+#name input that converts name into uppercase and slices it into list
+name = [*input("Enter name: ").upper()]
+
+#creating list with items that corresponds to name letters
+name_letters = [nato_dict[letter] for letter in name]
+
+#printing the result
+print(name_letters)
