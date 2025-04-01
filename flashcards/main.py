0f86f48cf111a66303162f05602dd82d86df7762
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-04-01 18:40:39 +0200

    fixes

--- flashcards/main.py
+++ flashcards/main.py
@@ -6,35 +6,73 @@ window = Tk()
 FONT_LANGUAGE = ('Ariel', 30, 'italic')
 FONT_WORD = ('Ariel', 60, 'bold')
 BG_COLOR = '#B1DDC6'
+wrong_words = []
+random_pick = ''
+should_remove = False
 
 #initial setup
 window.config(bg=BG_COLOR)
 canvas = Canvas(width=800, height=526, bg=BG_COLOR, highlightthickness=0)
 
 #-----------FUNCTIONS---------------
-def new_word():
-    data_df = pd.read_csv('data/french_words.csv')
+def open_file(): #checks if file with wrong words exists
+    global should_remove
+    try:
+        data_df = pd.read_csv('data/wrong_words.csv')
+        if len(data_df) < 1:
+            data_df = pd.read_csv('data/words.csv')
+        else:
+            should_remove = True
+    except FileNotFoundError:
+        data_df = pd.read_csv('data/words.csv')
+    finally:
+        return data_df
+
+
+def new_word(state):
+    global random_pick
+    global should_remove
+    data_df = open_file()
+
+    try:
+        if state == 'wrong' and random_pick != '':
+            to_add = [data_df.at[random_pick, 'French'], data_df.at[random_pick, 'English']]
+            if to_add not in wrong_words:
+                wrong_words.append(to_add)
+            print(wrong_words)
+        elif state == 'right' and random_pick != '':
+            if should_remove:
+                to_remove = [data_df.at[random_pick, 'French'], data_df.at[random_pick, 'English']]
+                if to_remove in wrong_words:
+                    wrong_words.remove(to_remove)
+                data_df = open_file()
+    except NameError:
+        print('Error')
 
     #picking random word
-    random_pick = random.randrange(1, len(data_df))
+    random_pick = random.randrange(0, len(data_df))
     foreign_word = data_df['French'][random_pick]
     english_word = data_df['English'][random_pick]
 
     #updating canvas with english words
-    update_to_english(english_word)
-    window.after(3000, lambda: update_to_foreign(foreign_word))
+    update_to_foreign(foreign_word)
+    window.after(3000, lambda: update_to_english(english_word))
+
+    if len(wrong_words) > 0:
+        wrong_df = pd.DataFrame(wrong_words, columns=['French', 'English'])
+        wrong_df.to_csv('data/wrong_words.csv', index=False, header=True)
 
 
 #updating canvas with foreign words after 3 seconds
 def update_to_foreign(foreign_word):
-    canvas.itemconfig(flashcard, image=flashcard_back)
-    canvas.itemconfig(word, text=foreign_word, fill='white')
-    canvas.itemconfig(language, text='French', fill='white')
+    canvas.itemconfig(flashcard, image=flashcard_front)
+    canvas.itemconfig(word, text=foreign_word, fill='black')
+    canvas.itemconfig(language, text='French', fill='black')
 
 def update_to_english(english_word):
-    canvas.itemconfig(flashcard, image=flashcard_front)
-    canvas.itemconfig(word, text=english_word, fill='black')
-    canvas.itemconfig(language, text='English', fill='black')
+    canvas.itemconfig(flashcard, image=flashcard_back)
+    canvas.itemconfig(word, text=english_word, fill='white')
+    canvas.itemconfig(language, text='English', fill='white')
 
 
 #------------IMAGES-----------------
@@ -46,8 +84,8 @@ wrong_image = PhotoImage(file='images/wrong.png')
 #------------BUTTONS-----------------
 front_flash = Button(image=flashcard_front)
 back_flash = Button(image=flashcard_back)
-right = Button(image=right_image, highlightthickness=0, command=new_word)
-wrong = Button(image=wrong_image, highlightthickness=0, command=new_word)
+right = Button(image=right_image, highlightthickness=0, command=lambda: new_word('right'))
+wrong = Button(image=wrong_image, highlightthickness=0, command=lambda: new_word('wrong'))
 
 #---------------UI-------------------
 flashcard = canvas.create_image(400, 263, image=flashcard_front)
@@ -58,6 +96,6 @@ wrong.grid(row=1, column=0)
 right.grid(row=1, column=1)
 
 #start
-new_word()
+new_word('none')
 
 window.mainloop()
