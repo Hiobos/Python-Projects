569a56ed3429089c4b75f32cac435ea77490e021
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-04-01 13:37:16 +0200

    updated file reading
    instead of simple open now it's using pandas module to load .csv data

--- flashcards/main.py
+++ flashcards/main.py
@@ -1,42 +1,40 @@
 from tkinter import *
 import random
-import time
+import pandas as pd
 
 window = Tk()
 FONT_LANGUAGE = ('Ariel', 30, 'italic')
 FONT_WORD = ('Ariel', 60, 'bold')
 BG_COLOR = '#B1DDC6'
 
+#initial setup
 window.config(bg=BG_COLOR)
 canvas = Canvas(width=800, height=526, bg=BG_COLOR, highlightthickness=0)
 
 #-----------FUNCTIONS---------------
 def new_word():
-    with open('data/french_words.csv', 'r') as data:
-        dict1 = [i.strip('\n') for i in data]
-        dict2 = [i.split(',') for i in dict1]
-        #print(dict[0][0]) to daje samo 'French'
+    data_df = pd.read_csv('data/french_words.csv')
 
-        #picking random word
-        random_foreign_word = dict2[random.randrange(1, len(dict1))]
-        foreign_word = random_foreign_word[0]
-        english_word = random_foreign_word[1]
+    #picking random word
+    random_pick = random.randrange(1, len(data_df))
+    foreign_word = data_df['French'][random_pick]
+    english_word = data_df['English'][random_pick]
 
-        #updating canvas with english words
-        update_to_english(english_word, dict2)
-        window.after(3000, lambda: update_to_foreign(foreign_word, dict2))
+    #updating canvas with english words
+    update_to_english(english_word)
+    window.after(3000, lambda: update_to_foreign(foreign_word))
 
 
 #updating canvas with foreign words after 3 seconds
-def update_to_foreign(foreign_word, dict2):
+def update_to_foreign(foreign_word):
     canvas.itemconfig(flashcard, image=flashcard_back)
-    canvas.itemconfig(word, text=foreign_word)
-    canvas.itemconfig(language, text=dict2[0][0])
+    canvas.itemconfig(word, text=foreign_word, fill='white')
+    canvas.itemconfig(language, text='French', fill='white')
 
-def update_to_english(english_word, dict2):
+def update_to_english(english_word):
     canvas.itemconfig(flashcard, image=flashcard_front)
-    canvas.itemconfig(word, text=english_word)
-    canvas.itemconfig(language, text=dict2[0][1])
+    canvas.itemconfig(word, text=english_word, fill='black')
+    canvas.itemconfig(language, text='English', fill='black')
 
 
 #------------IMAGES-----------------
@@ -50,9 +48,6 @@ front_flash = Button(image=flashcard_front)
 back_flash = Button(image=flashcard_back)
 right = Button(image=right_image, highlightthickness=0, command=new_word)
 wrong = Button(image=wrong_image, highlightthickness=0, command=new_word)
-
-
-
 
 #---------------UI-------------------
 flashcard = canvas.create_image(400, 263, image=flashcard_front)
